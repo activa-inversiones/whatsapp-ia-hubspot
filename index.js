cf13e7a3b21b221af235c8a964771a4b63ff127d
activa-inversiones <mcifu005@gmail.com>
2026-03-14 19:55:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1169,14 +1169,20 @@ async function zhBooksCreateEstimate(data, customer_name, phone) {
 
     // 2. Crear Estimate
     const line_items = data.items.map((it) => {
-  return {
-    item_id: ZOHO.DEFAULT_ITEM_ID,
-    description: `${it.product} ${it.measures} ${it.color || data.default_color || ""}`.trim(),
+
+  const item = {
+    name: `${it.product} ${(it.color || data.default_color || "")}`.trim(),
+    description: `${it.measures}`,
     rate: Number(it.unit_price || 0),
     quantity: Number(it.qty || 1)
   };
-});
 
+  if (ZOHO.DEFAULT_ITEM_ID) {
+    item.item_id = ZOHO.DEFAULT_ITEM_ID;
+  }
+
+  return item;
+});
 
     const estimatePayload = {
       customer_id,
